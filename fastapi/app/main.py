dbe1336691e46489ae5860664a5b8f824d5e8e87
rodionshapoval <r.shapoval@ukma.edu.ua>
2026-02-26 20:32:34 +0200

    Add error handling to the http client. Fix creating new http client each request. Correct the response model

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from schemas.request_models import Order
 from services.orders import process_order, import_csv
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/orders")
 async def list_orders(name : str = "Dmitriy"):
@@ -16,8 +25,7 @@ async def create_order_from_json(order: Order):
 
 @app.post("/orders/import")
 async def import_orders_from_csv(csv_file : UploadFile, encoding: str = "utf-8"):
-    #TODO рахувати кількість валідних order і сповіщувати користувача про це у відповіді
+    #TODO рахувати кількість валідних order і сповіщати користувача про це у відповіді
     await import_csv(csv_file, encoding)
 
     return {"message": "Orders imported"}
-
