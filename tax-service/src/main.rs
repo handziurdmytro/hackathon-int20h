8684e35b5ecae6a3b2cc8273ef3c44107d4cd1b0
Handziur Dmytro <handziurdmytro@gmail.com>
2026-02-26 20:30:39 +0200

    feat(tax-service): integrate tax and json parsing logic into main.rs

--- tax-service/src/main.rs
+++ tax-service/src/main.rs
@@ -1,16 +1,37 @@
 mod geo;
 mod geometry;
+mod tax;
 
+use crate::geo::{parse_all_data, Location};
+use crate::tax::{parse_csv, TaxRateRecord, TaxedOrder};
+use axum::extract::State;
 use axum::http::StatusCode;
 use axum::routing::{get, post};
 use axum::{Json, Router};
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::time::Instant;
 
 #[tokio::main]
 async fn main() {
+    let start = Instant::now();
+    println!("Start...");
+
+    let (state, cities, counties) = parse_all_data();
+    let taxes = parse_csv();
+
+    let shared_state = AppState {
+        state: Arc::new(state),
+        cities: Arc::new(cities),
+        counties: Arc::new(counties),
+        taxes: Arc::new(taxes),
+    };
+
     let app = Router::new()
         .route("/", get(root))
-        .route("/tax", post(handle_task));
+        .route("/tax", post(handle_task))
+        .with_state(shared_state);
 
     /*TODO: add port to config files*/
     let port = 3030u16;
@@ -19,6 +40,7 @@ async fn main() {
         .await
         .unwrap();
 
+    println!("BOOM!!! Elapsed: {:?}", start.elapsed());
     axum::serve(listener, app).await.unwrap();
 }
 
@@ -26,9 +48,19 @@ async fn root() -> String {
     "OK\n".to_string()
 }
 
-async fn handle_task(Json(payload): Json<Order>) -> Result<(StatusCode, Json<TaxedOrder>), StatusCode> {
-    let tax = geo::calculate_tax(payload.latitude, payload.longitude, payload.subtotal);
-    Ok((StatusCode::OK, Json(TaxedOrder{tax})))
+async fn handle_task(
+    State(app_state): State<AppState>,
+    Json(payload): Json<Order>,
+) -> Result<(StatusCode, Json<TaxedOrder>), StatusCode> {
+    Ok((
+        StatusCode::OK,
+        Json(tax::calculate_tax(
+            payload.latitude,
+            payload.longitude,
+            payload.subtotal,
+            &app_state,
+        )?),
+    ))
 }
 
 #[derive(Deserialize, Debug)]
@@ -37,9 +69,10 @@ struct Order {
     longitude: f64,
     subtotal: f64,
 }
-
-#[derive(Serialize, Debug)]
-struct TaxedOrder {
-    tax: f64,
-    /* TODO: add more data */
+#[derive(Debug, Clone)]
+struct AppState {
+    pub state: Arc<Location>,
+    pub cities: Arc<Location>,
+    pub counties: Arc<Location>,
+    pub taxes: Arc<HashMap<String, TaxRateRecord>>,
 }
